8371be5c58bca14a3a7d106dd470f3950fbe81e9
thanhtam.nguyen <thanhtam.nguyen@MU-C1-08.mullen.pvt.k12.co.us>
2024-09-27 12:10:17 -0600

    initialize Commit

---
+++ reverseWord/src/Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Reverse
+	{
+		public static void main(String[] args)
+			{
+				Scanner userInput = new Scanner(System.in);
+				System.out.println("Please input a random word.");
+				
+				String word = userInput.nextLine();
+				
+				String[] array = word.split("");
+				for (int i = array.length - 1; i >= 0; i--)
+					{
+						System.out.print(array[i]);
+					}
+			}
+	}
